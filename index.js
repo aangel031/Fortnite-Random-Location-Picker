905b1f990a38a286cc3abe9eb89b0bce01fb2956
AlexHGaming <lightweightagarioclan@gmail.com>
2018-03-01 11:07:07 +0000

    New city = new update

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ var locations = [
 "Wailing Woods",
 "Lonely Lodge",
 "Retail Row",
-"Moisty Mire"
+"Moisty Mire",
+ "Lucky Landing
 //.sort() puts them in order.
 ];
 
@@ -56,6 +57,7 @@ if (randomise == 15) randomise="Wailing Woods";
 if (randomise == 16) randomise="Lonely Lodge";
 if (randomise == 17) randomise="Retail Row";
 if (randomise == 18) randomise="Moisty Mire";
+ if (randomise == 18) randomise="Lucky Landing";
 if (randomise <= 0 || randomise >= 19) randomise == "[UNDETECTED] - Report this to <a href = 'https://github.com/AlexHGaming/Fortnite-Random-Place-Generator'>the GitHub repo and make an issue screenshotting your console.</a>"
 document.getElementById("mout").innerHTML = `<code><center><h1>${randomise}</code>`
 };
