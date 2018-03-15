e3081291d6b3de6e431e78a3e4ee78401086c7ce
AlexHGaming <lightweightagarioclan@gmail.com>
2018-03-15 21:32:05 +0000

    Update allcities.js

--- allcities.js
+++ allcities.js
@@ -1,8 +1,9 @@
 function allcities() {
     var text = "";
+    var lastlocation = "Wailing Woods";
     for (var i = 0; i < ll; i++) {
           text += locations.sort()[i] + ", ";
-    document.getElementById("op").innerHTML = `<br>${text}`;
+    document.getElementById("op").innerHTML = `<br>${text} and ${lastlocation}.`;
     }
     console.log("User requested to show the total city list.")
     }
