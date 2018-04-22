7c2a48743d38ab15602013f2f7a65f8fb6b4716f
AlexHGaming <lightweightagarioclan@gmail.com>
2018-04-22 15:53:55 +0100

    Add files via upload

--- JS/allcities.js
+++ JS/allcities.js
@@ -19,16 +19,18 @@
     "Lucky Landing"
     //.sort() puts them in order.
     ];
-var lastlocation = "Wailing Woods"
+var lastlocation = "Wailing Woods";
     var ll = locations.length;
     console.log("Total cities listed in the array: " + ll)
 
 function allcities() {
+
+    var outputcolour = "#FF0000"
     
     var text = "";
     for (var i = 0; i < ll; i++) {
           text += locations.sort()[i] + ", ";
-    document.getElementById("op").innerHTML = `<br>${text} ${lastlocation}.`;
+    document.getElementById("op").innerHTML = `<br>${text} ${lastlocation}.<br><br><font color = '#FFFFFF'>${ll} cities alltogether.</font>`;
     }
     console.log("User requested to show the total city list.")
     }
---
+++ JS/colours.js
@@ -0,0 +1,11 @@
+function main2() {
+
+
+var a = document.getElementById("colour1").value;
+    var b = document.getElementById("colour2").value;
+    var c = document.getElementById("colour3").value;
+
+    document.getElementById("mout").style = `font-size: 60px; color: ${a};`;
+    document.getElementById("op").style = `font-size:20px; color: ${b};`;
+document.getElementById("headercolour").style = `background-color:${c}`;
+}
